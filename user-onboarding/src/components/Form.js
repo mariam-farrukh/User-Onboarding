5ae07fa781affea9fa2aeaa16df79530a8d71120
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-08-07 15:37:40 -0500

    Updated styling

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -24,39 +24,45 @@ const NewUserForm = ({errors, touched, values, status }) => {
 
     return (
         <div>
-            <h1>New User Form</h1>
-            <Form>
-                <Field type="text" name="firstname" placeholder="First Name"/>
-                    {touched.firstname && errors.firstname && (
-                        <p className="error">{errors.firstname}</p>
-                    )}
+            <div className='user-form'>
+                <h1>New User Form</h1>
+                <Form>
+                    <Field type="text" name="firstname" placeholder="First Name"/>
+                        {touched.firstname && errors.firstname && (
+                            <p className="error">{errors.firstname}</p>
+                        )}
 
-                <Field type="text" name="lastname" placeholder="Last Name"/>
-                    {touched.lastname && errors.lastname && (
-                        <p className="error">{errors.lastname}</p>
-                    )}
+                    <Field type="text" name="lastname" placeholder="Last Name"/>
+                        {touched.lastname && errors.lastname && (
+                            <p className="error">{errors.lastname}</p>
+                        )}
 
-                <Field type="email" name="email" placeholder="Email"/>
-                    {touched.email && errors.email && 
-                        (<p className="error">{errors.email}</p>)}
+                    <Field type="email" name="email" placeholder="Email"/>
+                        {touched.email && errors.email && 
+                            (<p className="error">{errors.email}</p>)}
 
-                <Field type="password" name="password" placeholder="Password"/>
-                    {touched.password && errors.password && 
-                        (<p className="error">{errors.password}</p>)}
+                    <Field type="password" name="password" placeholder="Password"/>
+                        {touched.password && errors.password && 
+                            (<p className="error">{errors.password}</p>)}
 
-                <label>
-                    Terms of Service
-                    <Field
-                        type="checkbox"
-                        name="termsofservice"
-                        checked={values.termsofservice}
-                    />
-                </label>
-                <button type="submit">Submit</button>
-            </Form>
-            {users.map(user => (
-                <p key={user.id}>{user.name}</p>
-            ))}
+                    <label className="checkbox-container">
+                        Terms of Service
+                        <Field 
+                            type="checkbox"
+                            name="termsofservice"
+                            checked={values.termsofservice}
+                        />
+                        <span className="checkmark" />
+                    </label>
+                    <button type="submit">Submit</button>
+                </Form>
+            </div>
+            <div>
+                <h2>Users:</h2>
+                    {users.map(user => (
+                        <p key={user.id}>{user.firstname} {user.lastname}</p>
+                    ))}
+            </div>
         </div>
     );
 };
