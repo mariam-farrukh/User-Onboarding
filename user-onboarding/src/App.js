67e933772e481a34ac1b211ed8a8ab452f48aa8f
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-08-07 14:30:16 -0500

    Setting up form with yup and formik

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import NewUserForm from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      Testing
+      <NewUserForm/>
     </div>
   );
 }
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Form } from 'formik';
+import { Form, Field, withFormik } from 'formik';
 import * as Yup from "yup";
 
 // We want to create a form to onboard a new user to our system. 
@@ -12,3 +12,56 @@ import * as Yup from "yup";
 // - Terms of Service (checkbox)
 // - A Submit button to send our form data to the server.
 
+const NewUserForm = ({errors, touched, values, status }) => {
+    const [newUsers, setNewUsers] = useState([]);
+    console.log(newUsers);
+
+    useEffect(() => {
+        if (status) {
+            setNewUsers([...newUsers, status])
+        }
+    }, [status]);
+
+    return (
+        <div>
+            <h1>New User Form</h1>
+            <Form>
+                <Field type="text" name="name" placeholder="name"/>
+
+                <Field type="email" name="email" placeholder="Email"/>
+
+                <Field type="password" name="password" placeholder="Password"/>
+
+                <label>
+                    Terms of Service
+                    <Field
+                        type="checkbox"
+                        name="termsofservice"
+                        checked={nothing}
+                    />
+                </label>
+                <button type="submit">Submit</button>
+            </Form>
+        </div>
+    );
+};
+
+const FormikNewUserForm = withFormik({
+    mapPropsToValues({ name, email, password, termsofservice }) {
+        return {
+            name: name || '',
+            email: email || '',
+            password: password || '',
+            termsofservice: termsofservice || ''
+        };
+    },
+
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required('Enter your name'),
+        email: Yup.string().required(),
+        password: Yup.string().required()
+    }),
+
+})
+
+export default NewUserForm;
